55e38ac186d7add73c42cf3729422971db89ff5d
Sabri_Efe <se.zybk@hotmail.com>
2026-06-30 18:36:02 +0300

    files fixed

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -8,6 +8,7 @@ export const initMongoConnection = async () => {
     MONGODB_DB,
   } = process.env;
 
+
   const mongoUri = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?ssl=true&replicaSet=atlas-46vzjn-shard-0&authSource=admin&appName=Cluster0`;
 
   try {
